1e15db14fdaff32ac73a2aa36984ec0917d016f6
noah attia <noah.attia@gmail.com>
2024-05-31 13:47:48 +0200

    starting project

---
+++ src/react/components/Exemple/Exemple.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Exemple = () => {
+  return (
+    <div className="p-4 bg-blue-500 text-white">
+      Hello, this is a React component with Tailwind CSS!
+    </div>
+  );
+};
+
+export default Exemple;
