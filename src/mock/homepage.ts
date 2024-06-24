9dd38ed106c2582a50ad4618578cecd7c9b1ee56
noah attia <noah.attia@gmail.com>
2024-06-24 13:35:32 +0200

    first push layout

---
+++ src/mock/homepage.ts
@@ -0,0 +1,250 @@
+export const homepage = {
+  banner: {
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem ?',
+    link: "M'inscrire",
+    button: "M'inscrire",
+    visible: true,
+  },
+  nav: {
+    logo: '8821a0b1-f1e6-4557-88d5-747260ad067e.png',
+    navmenu: [
+      {
+        label: 'Formations',
+        href: '/',
+      },
+      {
+        label: "L'école",
+        href: '/',
+      },
+      {
+        label: 'Admissions',
+        href: '/',
+      },
+      {
+        label: 'Entreprises',
+        href: '/',
+      },
+    ],
+    button: 'Contactez-nous',
+  },
+  header: {
+    title: 'Formations <b>certifiantes</b> <br> 100 % à <b>distance</b>',
+    subtitle:
+      'Envie de développer vos compétences et de faire évoluer votre carrière rapidement ?',
+    pictures: [
+      {
+        src: '05d1619d-965d-45d8-a42c-215bddd50095.jpg',
+        alt: 'Lorem ipsum dolor sit amet consectetur adipiscing eliteus',
+      },
+      {
+        src: 'c75cb5b9-9a39-4e16-b00a-ab1baaa0d72b.jpg',
+        alt: 'Lorem ipsum dolor sit amet consectetur adipiscing eliteus',
+      },
+      {
+        src: '97c96dbb-2e1e-4cc0-97a9-f8e3581fd06c.jpg',
+        alt: 'Lorem ipsum dolor sit amet consectetur adipiscing eliteus',
+      },
+      {
+        src: '5215f795-2319-43cc-8c02-710f436d005c.jpg',
+        alt: 'Lorem ipsum dolor sit amet consectetur adipiscing eliteus',
+      },
+    ],
+  },
+  formation: {
+    title: 'Découvrez nos bachelors',
+    subtitle:
+      "Intégrez l’une de nos formations à distance et obtenez un diplôme européen de premier cycle d'études supérieures de type bachelor’s degree",
+    formations: [
+      {
+        cardTitle: 'Bachelor Immobilier',
+        cardDescription:
+          "Le secteur de l'immobilier vous passionne et vous souhaitez prendre des responsabilités...",
+        cardFullDescription:
+          "Le secteur de l'immobilier vous passionne et vous souhaitez prendre des responsabilités au sein d'une agence immobilière ou d'un syndic de copropriété ? ",
+        level: 'Niveau 6 ou Bac+3',
+        button: 'En savoir plus',
+        link: '/formation/immobilier-gestion',
+        image: '8627d78b-a102-43ba-b4ec-ad1eb1fa702d.jpeg',
+        chip: '',
+      },
+      {
+        cardTitle: 'Bachelor Immobilier',
+        cardDescription:
+          "Le secteur de l'immobilier vous passionne et vous souhaitez prendre des responsabilités...",
+        cardFullDescription:
+          "Le secteur de l'immobilier vous passionne et vous souhaitez prendre des responsabilités au sein d'une agence immobilière ou d'un syndic de copropriété ? ",
+        level: 'Niveau 6 ou Bac+3',
+        button: 'En savoir plus',
+        link: '/formation/immobilier-transaction',
+        image: '8627d78b-a102-43ba-b4ec-ad1eb1fa702d.jpeg',
+        chip: '',
+      },
+      {
+        cardTitle: 'Bachelor Bien-être',
+        cardDescription:
+          'Vous souhaitez devenir un expert en Spa Business Management ? Cette formation vous permet...',
+        cardFullDescription:
+          'Vous souhaitez devenir un expert en Spa Business Management ? Cette formation vous permet de développer toutes les compétences requises pour exercer ce métier.',
+        level: 'Niveau 6 ou Bac+3',
+        button: 'En savoir plus',
+        link: '/formation/bien-etre',
+        image: '6802e599-d16b-4fab-b323-919ea8f2777e.jpg',
+        chip: '',
+      },
+      {
+        cardTitle: 'Bachelor Bien-être',
+        cardDescription:
+          'Vous souhaitez devenir un expert en Spa Business Management ? Cette formation vous permet...',
+        cardFullDescription:
+          'Vous souhaitez devenir un expert en Spa Business Management ? Cette formation vous permet de développer toutes les compétences requises pour exercer ce métier.',
+        level: 'Niveau 6 ou Bac+3',
+        button: 'En savoir plus',
+        link: '/formation/marketing',
+        image: '6802e599-d16b-4fab-b323-919ea8f2777e.jpg',
+        chip: '',
+      },
+    ],
+  },
+  warranty: {
+    title: 'Les garanties CNFDi CAMPUS',
+    pictos: [
+      {
+        pictoTitle: 'Objectif emploi pour tous',
+        picto: '12cdf74b-4ad4-4532-85a2-f0b91f8a9e9f.png',
+        color: '#0A6FFD',
+        visible: true,
+      },
+      {
+        pictoTitle: 'Équipe pédagogique disponible',
+        picto: '2f38cd18-e287-419d-b159-783bb3993a6e.png',
+        color: '#6C757D',
+        visible: true,
+      },
+      {
+        pictoTitle: "Inscription toute l'année",
+        picto: 'b40b8a78-cc03-493a-a3c8-0b9ebec27470.png',
+        color: '#198755',
+        visible: true,
+      },
+      {
+        pictoTitle: 'Admission hors Parcoursup',
+        picto: 'a9f23e20-78ea-47db-9520-8d300f7153ab.png',
+        color: '#DC3444',
+        visible: true,
+      },
+      {
+        pictoTitle: 'Espace élève en ligne',
+        picto: 'dece89f0-7947-4879-a589-fc0414e3e5f0.png',
+        color: '#FFC106',
+        visible: true,
+      },
+      {
+        pictoTitle: 'Formations reconnues',
+        picto: 'be43d5d6-2cdf-462f-aaf1-4be1a94cf51d.png',
+        color: '#10CAF0',
+        visible: true,
+      },
+      {
+        pictoTitle: 'Financement adapté',
+        picto: 'a654b35a-be52-4a33-9a80-5c735c025328.png',
+        color: '#ab8209',
+        visible: true,
+      },
+      {
+        pictoTitle: "Poursuite d'études possible",
+        picto: 'd1d8deb7-2f54-49e4-9ee9-71487c7dc59f.png',
+        color: '#f74d19',
+        visible: true,
+      },
+    ],
+  },
+  reviews: {
+    title: 'NOS élèves parlent du cnfdi',
+    visible: true,
+  },
+  enterprise: {
+    title: 'ENTREPRISES',
+    subtitle:
+      'Développez les <strong>compétences</strong> de vos <strong>salariés</strong> et renforcez vos <strong>équipes</strong>',
+    enterprises: [
+      {
+        title: 'Formez vos collaborateurs !',
+        subtitle: 'Des solutions sur mesure pour votre entreprise',
+        img1: '00fac69a-f02d-4ee2-badb-d936c81ec0de.jpeg',
+        link: '/entreprise/formez-vos-collaborateurs',
+        visible: true,
+      },
+      {
+        title: 'Propositions de stage',
+        subtitle: 'Accueillir nos stagiaires et poster vos offres de stages',
+        img1: '898123e9-7c27-4756-87f0-d077e40046ed.jpeg',
+        link: '/entreprise/proposition-de-stage',
+        visible: true,
+      },
+    ],
+    button: 'En savoir plus',
+    visible: true,
+  },
+  stat: {
+    stats: [
+      {
+        title: '+ de 31 ans',
+        subtitle: 'd’existence',
+        visible: true,
+      },
+      {
+        title: '+ de 150 000',
+        subtitle: 'élèves formés <span>depuis 2014</span>',
+        visible: true,
+      },
+      {
+        title: '93%',
+        subtitle: 'd’élèves satisfaits de leur cours en 2022',
+        visible: true,
+      },
+      {
+        title: '96,84%',
+        subtitle: 'de réussite sur 5417  éléves en 2022',
+        visible: true,
+      },
+    ],
+    visible: true,
+  },
+  partner: {
+    title: 'Ils nous font confiance',
+    subtitle:
+      'Décrochez une <strong>certification</strong> avec nos <strong>partenaires</strong> institutionnels',
+    partners: [
+      {
+        logo: 'd796e933-1d9d-4bf0-8a4c-3bf34f30b86b.png',
+        link: 'https://www.moncompteformation.gouv.fr/',
+        visible: true,
+      },
+      {
+        logo: 'fd542cec-5344-4153-9be8-7226f0c5a72c.png',
+        link: 'https://www.opcoep.fr/',
+        visible: true,
+      },
+      {
+        logo: 'd029e305-fec6-4094-a3e2-83d67587095f.png',
+        visible: true,
+        link: 'https://www.pole-emploi.fr/',
+      },
+      {
+        visible: true,
+        logo: 'a97b4a72-78aa-4805-9a11-c747dab8fed6.png',
+        link: 'https://travail-emploi.gouv.fr/formation-professionnelle/acteurs-cadre-et-qualite-de-la-formation-professionnelle/article/qualiopi-marque-de-certification-qualite-des-prestataires-de-formation',
+      },
+      {
+        visible: true,
+        logo: '38bc3e28-5e60-4c56-b0e1-b7e51a0acf6b.png',
+        link: 'https://www.fede.education',
+      },
+    ],
+  },
+
+  footer: {
+    title: 'FOOTER',
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}',],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: '320px',
+        tablet: '768px',
+        desktop: '1280px',
+      },
+      fontFamily: {
+        'roboto-regular': ['Roboto-Regular', 'sans-serif'],
+        // Add other font families if needed
+      },
+      colors: {
+        primary: '#007aaa',
+        secondary: '#f7bf1a',
+      },
+    },
   },
   plugins: [],
 }
