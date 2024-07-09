7acd12a4d5f3e5041691c25bdc94c9efc7c0209f
noah attia <noah.attia@gmail.com>
2024-07-09 18:05:58 +0200

    update comapreteru

--- src/react/components/FighterImage/FighterImage.tsx
+++ src/react/components/FighterImage/FighterImage.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const FighterImage = ({ fighter }) => {
-  const imageUrl = fighter.images.find(img => img.type === 'background')?.url || fighter.images[1].url;
+  
+console.log(fighter);
+  let profileImage = fighter.images[0]?.url;
+
+  if (!profileImage || profileImage.includes("no-profile-image.png") || profileImage.includes("silhouette-headshot-female.png")) {
+    // Use fallback image if no profile image or if it's the "no-profile-image"
+    profileImage = fighter.weight_class.includes("Women's ") ? '/UFC-Female-Fallback-Image.jpg' : '/UFC-Male-Fallback-Image.jpg';
+  }
   const statusColor = fighter.status === 'active' ? 'text-green-500' : 'text-red-500';
 
   return (
-    <div className="flex flex-col items-center text-center p-4 bg-gray-100 rounded-lg shadow-md">
+    <div className="flex flex-col items-center text-center p-4 bg-gray-100 rounded-lg shadow-md h-[460px]">
       <div className="relative w-48 h-72 overflow-hidden mb-4">
-        {imageUrl && <img src={imageUrl} alt={fighter.name} className="w-full h-full object-cover object-top transform rotate-6" />}
+        {profileImage && <img src={profileImage} alt={fighter.name} className="w-full h-full object-cover object-top transform " />}
       </div>
       <h2 className="text-xl font-bold">{fighter.name}</h2>
       {fighter.nickname && <p className="italic text-gray-700">"{fighter.nickname}"</p>}
--- src/react/containers/Comparateur/Comparateur.tsx
+++ src/react/containers/Comparateur/Comparateur.tsx
@@ -6,11 +6,16 @@ import FighterImage from '../../components/FighterImage';
 const CompareFighters = () => {
   const [fighter1, setFighter1] = useState(null);
   const [fighter2, setFighter2] = useState(null);
+  const [showStatistics, setShowStatistics] = useState(false);
 
   useEffect(() => {
     console.log('fighter1', fighter1);
     console.log('fighter2', fighter2);
   }, [fighter1, fighter2]);
+
+  const handlePredictFight = () => {
+    setShowStatistics(true);
+  };
 
   return (
     <div className="flex flex-col items-center p-5">
@@ -18,33 +23,38 @@ const CompareFighters = () => {
       <div className="flex flex-col md:flex-row items-center justify-center gap-2">
         <SearchBarFighter onFighterSelect={setFighter1} />
         <div className="text-2xl font-bold">VS</div>
-      <SearchBarFighter onFighterSelect={setFighter2} />
+        <SearchBarFighter onFighterSelect={setFighter2} />
       </div>
-      <div className="flex justify-around items-center w-full mt-6 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-around items-center w-full max-w-3xl mt-6 gap-4">
         {fighter1 && (
           <div className="flex flex-col items-center">
-            <FighterImage fighter={fighter1} className="rounded-lg shadow-md" />
+            <FighterImage fighter={fighter1}  />
           </div>
         )}
         {fighter2 && (
           <div className="flex flex-col items-center">
-            <FighterImage fighter={fighter2} className="rounded-lg shadow-md" />
+            <FighterImage fighter={fighter2}  />
           </div>
         )}
       </div>
-      {fighter2 && (
-          <button className="bg-blue-500 hover:bg-blue-600 text-white text-lg font-semibold py-2 px-4 rounded-lg mt-4">
-            Prédire le combat
-          </button>
-        )}
-      <div className="w-full mt-6">
-        <AthleteStatisticsDuo 
-          stats1={fighter1?.career_statistics[0]} 
-          stats2={fighter2?.career_statistics[0]} 
-          name1={fighter1?.name}  
-          name2={fighter2?.name}
-        />
-      </div>
+      {fighter1 && fighter2 && (
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white text-lg font-semibold py-2 px-4 rounded-lg mt-4"
+          onClick={handlePredictFight}
+        >
+          Prédire le combat
+        </button>
+      )}
+      {showStatistics && (
+        <div className="w-full mt-6">
+          <AthleteStatisticsDuo 
+            stats1={fighter1?.career_statistics[0]} 
+            stats2={fighter2?.career_statistics[0]} 
+            name1={fighter1?.name}  
+            name2={fighter2?.name}
+          />
+        </div>
+      )}
     </div>
   );
 };
