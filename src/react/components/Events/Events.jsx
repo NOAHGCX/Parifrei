a80c2ea705189cae2ab753ff65c1b25dc32e8bbe
Astrostellar06 <laubertie.clement@gmail.com>
2024-07-09 19:12:41 +0200

    Events

--- src/react/components/Events/Events.jsx
+++ src/react/components/Events/Events.jsx
@@ -8,7 +8,7 @@ import { useAstroQuery } from '../../helpers/apollo';
 const GET_UPCOMING_EVENTS = gql`
   query GetUpcomingEvents {
     events(
-      where: { date: { _gte: "2024-05-01" } }
+      where: { date: { _gte: "2017-05-01" } }
       order_by: { date: asc }
     ) {
       id
@@ -52,15 +52,17 @@ const UpcomingEvents = () => {
       <h1 className="text-4xl font-bold text-center mb-8">Prochains événements</h1>
       <div className="flex flex-wrap justify-center">
         {data.events.map(event => (
-          <div key={event.id} className="bg-white rounded-lg shadow-lg m-4 max-w-xs transition-transform transform hover:-translate-y-1">
-            {/* Exemple d'image */}
-            <img src="https://via.placeholder.com/400x225" alt={event.event_name} className="w-full h-48 object-cover rounded-t-lg" />
-            <div className="p-4">
-              <h2 className="text-xl font-semibold mb-2">{event.event_name}</h2>
-              <p className="text-gray-600 mb-1">{event.date}</p>
-              <p className="text-gray-600">{event.location}</p>
+          <a href={`/Prochains-evenements/${event.id}`}>
+            <div key={event.id} className="bg-white rounded-lg shadow-lg m-4 max-w-xs transition-transform transform hover:-translate-y-1">
+              {/* Exemple d'image */}
+              <img src="https://via.placeholder.com/400x225" alt={event.event_name} className="w-full h-48 object-cover rounded-t-lg" />
+              <div className="p-4">
+                <h2 className="text-xl font-semibold mb-2">{event.event_name}</h2>
+                <p className="text-gray-600 mb-1">{event.date}</p>
+                <p className="text-gray-600">{event.location}</p>
+              </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
